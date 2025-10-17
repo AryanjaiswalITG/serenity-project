2c9c0f3cd2e17c6c3da8201a21c3418b8093ba24
aryanjaiswalITG <aryanjaiswal@ITG.com>
2025-10-17 18:57:36 +0530

    17-10-2025

--- script/script.js
+++ script/script.js
@@ -6,10 +6,6 @@ var swiper = new Swiper(".mySwiper", {
 var swiper2 = new Swiper(".swiper_grid_4", {
     slidesPerView: 1,
     spaceBetween: 10,
-    navigation: {
-        nextE1: ".swiper-button-next",
-        nextE2: ".swiper-button-prev",
-    },
     breakpoints: {
         640: {
             slidesPerView: 2,
@@ -23,5 +19,30 @@ var swiper2 = new Swiper(".swiper_grid_4", {
             slidesPerView: 4,
             spaceBetween: 50,
         },
+    },
+});
+var swiper3 = new Swiper(".swiper_grid_4_2", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+        },
+    },
+});
+var review_swiper = new Swiper(".review-swiper", {
+    navigation: {
+        nextEl: ".for-next",
+        prevEl: ".for-prev",
     },
 });
